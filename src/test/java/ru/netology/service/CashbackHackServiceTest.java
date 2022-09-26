47366065cd5098c5185dd136fcce8c2120083f2c
anastasiynovaya <aefremova3014@gmail.com>
2022-09-26 12:19:19 +0500

    Chanded

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -6,25 +6,27 @@ import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
     @Test
-        public void Junit () {
-            CashbackHackService service = new CashbackHackService();
-            int expected = 500;
-            int actual = service.remain(500);
-            assertEquals(actual, expected);
-        }
-        @Test
-        public void testJunitRemainLarge() {
-            CashbackHackService service = new CashbackHackService();
-            int expected = 900;
-            int actual = service.remain(4100);
-            assertEquals(actual, expected);
-        }
+    public void Junit() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 500;
+        int actual = service.remain(500);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testJunitRemainLarge() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 900;
+        int actual = service.remain(4100);
+        assertEquals(expected, actual);
+    }
+
     @Test
     public void testJunitNull() {
         CashbackHackService service = new CashbackHackService();
         int expected = 0;
         int actual = service.remain(2000);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    }
+}
